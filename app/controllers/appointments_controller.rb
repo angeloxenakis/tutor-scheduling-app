a4ad43011e0f4e9b7e9465c5ba2507fcfff9fc7d
angeloxenakis <angeloxenakis@icloud.com>
2020-04-14 16:46:00 -0500

    all appointment curd except for date, time, and validation rules

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,25 +1,48 @@
 class AppointmentsController < ApplicationController 
     def index 
-
+        @appointments = Appointment.all
     end
 
     def show
-
+        @appointment = Appointment.find(params[:id])
     end
 
     def new
-
+        @appointment = Appointment.new
+        @students = Student.all
+        @locations = Location.all
+        @tutors = Tutor.all
+        @subjects = Subject.all
     end
 
     def create 
-
+        @appointment = Appointment.create(appointment_params)
+        redirect_to appointment_path(@appointment)
     end
 
     def edit
-
+        @appointment = Appointment.find(params[:id])
+        @students = Student.all
+        @locations = Location.all
+        @tutors = Tutor.all
+        @subjects = Subject.all
     end
 
     def update
+        @appointment = Appointment.find(params[:id])
+        @appointment.update(appointment_params)
+        redirect_to appointment_path(@appointment)
+    end
 
+    def destroy
+        @appointment = Appointment.find(params[:id])
+        @appointment.destroy
+        redirect_to appointments_path(@appointments)
+    end 
+
+    private
+
+    def appointment_params
+        params.required(:appointment).permit(:student_id, :tutor_id, :subject_id, :location_id)
     end
 end 
